d5701f13078124526d0d9227b08ae8aad4650f2f
Hector Romero <hector.romero@mataro.epiaedu.cat>
2016-12-27 23:01:41 +0100

    Archivo javascript

---
+++ js/funciones.js
@@ -0,0 +1,51 @@
+		
+			function presentacion1() {
+				document.getElementById("aficiones_texto").style.display ='none';
+				document.getElementById("aficiones_tabla").style.display ='none';
+				document.getElementById("gustos_texto").style.display ='none';
+				document.getElementById("gustos_tabla").style.display ='none';
+				document.getElementById("presentacion").style.display ='initial';
+				
+
+				}
+			function aficiones1() {
+				document.getElementById("presentacion").style.display ='none';
+				document.getElementById("gustos_texto").style.display ='none';
+				document.getElementById("gustos_tabla").style.display ='none';
+				document.getElementById("aficiones_texto").style.display ='initial';
+				document.getElementById("aficiones_tabla").style.display ='initial';
+
+				}
+			function gustos1() {
+				document.getElementById("presentacion").style.display ='none';
+				document.getElementById("aficiones_texto").style.display ='none';
+				document.getElementById("aficiones_tabla").style.display ='none';
+				document.getElementById("gustos_texto").style.display ='initial';
+				document.getElementById("gustos_tabla").style.display ='initial';
+
+				}
+			function presentacion2() {
+				document.getElementById("aficiones_texto1").style.display ='none';
+				document.getElementById("aficiones_tabla1").style.display ='none';
+				document.getElementById("gustos_texto1").style.display ='none';
+				document.getElementById("gustos_tabla1").style.display ='none';
+				document.getElementById("presentacion1").style.display ='initial';
+				
+
+				}
+			function aficiones2() {
+				document.getElementById("presentacion1").style.display ='none';
+				document.getElementById("gustos_texto1").style.display ='none';
+				document.getElementById("gustos_tabla1").style.display ='none';
+				document.getElementById("aficiones_texto1").style.display ='initial';
+				document.getElementById("aficiones_tabla1").style.display ='initial';
+
+				}
+			function gustos2() {
+				document.getElementById("presentacion1").style.display ='none';
+				document.getElementById("aficiones_texto1").style.display ='none';
+				document.getElementById("aficiones_tabla1").style.display ='none';
+				document.getElementById("gustos_texto1").style.display ='initial';
+				document.getElementById("gustos_tabla1").style.display ='initial';
+
+				}
