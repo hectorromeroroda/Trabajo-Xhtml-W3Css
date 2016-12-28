2fb033f67927a9b3392961fb3bd3f64a5f6264bf
Hector Romero <hector.romero@mataro.epiaedu.cat>
2016-12-28 17:32:04 +0100

    Modificaciones varias en Index.html y en funciones.js

--- js/funciones.js
+++ js/funciones.js
@@ -6,16 +6,23 @@
 				document.getElementById("gustos_tabla").style.display ='none';
 				document.getElementById("presentacion").style.display ='initial';
 				
-
+				document.getElementById("pres").className = "w3-grey";
+				document.getElementById("afic").classList.remove('w3-grey');
+				document.getElementById("gust").classList.remove('w3-grey');
 				}
+				
 			function aficiones1() {
 				document.getElementById("presentacion").style.display ='none';
 				document.getElementById("gustos_texto").style.display ='none';
 				document.getElementById("gustos_tabla").style.display ='none';
 				document.getElementById("aficiones_texto").style.display ='initial';
 				document.getElementById("aficiones_tabla").style.display ='initial';
-
+				
+				document.getElementById("afic").className = "w3-grey";
+				document.getElementById("pres").classList.remove('w3-grey');
+				document.getElementById("gust").classList.remove('w3-grey');
 				}
+				
 			function gustos1() {
 				document.getElementById("presentacion").style.display ='none';
 				document.getElementById("aficiones_texto").style.display ='none';
@@ -23,7 +30,14 @@
 				document.getElementById("gustos_texto").style.display ='initial';
 				document.getElementById("gustos_tabla").style.display ='initial';
 
+				document.getElementById("gust").className = "w3-grey";
+				document.getElementById("pres").classList.remove('w3-grey');
+				document.getElementById("afic").classList.remove('w3-grey');
 				}
+				
+				
+				
+				
 			function presentacion2() {
 				document.getElementById("aficiones_texto1").style.display ='none';
 				document.getElementById("aficiones_tabla1").style.display ='none';
@@ -31,16 +45,23 @@
 				document.getElementById("gustos_tabla1").style.display ='none';
 				document.getElementById("presentacion1").style.display ='initial';
 				
-
+				document.getElementById("pres1").className = "w3-grey";
+				document.getElementById("afic1").classList.remove('w3-grey');
+				document.getElementById("gust1").classList.remove('w3-grey');
 				}
+				
 			function aficiones2() {
 				document.getElementById("presentacion1").style.display ='none';
 				document.getElementById("gustos_texto1").style.display ='none';
 				document.getElementById("gustos_tabla1").style.display ='none';
 				document.getElementById("aficiones_texto1").style.display ='initial';
 				document.getElementById("aficiones_tabla1").style.display ='initial';
-
+				
+				document.getElementById("afic1").className = "w3-grey";
+				document.getElementById("pres1").classList.remove('w3-grey');
+				document.getElementById("gust1").classList.remove('w3-grey');
 				}
+				
 			function gustos2() {
 				document.getElementById("presentacion1").style.display ='none';
 				document.getElementById("aficiones_texto1").style.display ='none';
@@ -48,4 +69,7 @@
 				document.getElementById("gustos_texto1").style.display ='initial';
 				document.getElementById("gustos_tabla1").style.display ='initial';
 
+				document.getElementById("gust1").className = "w3-grey";
+				document.getElementById("pres1").classList.remove('w3-grey');
+				document.getElementById("afic1").classList.remove('w3-grey');
 				}
