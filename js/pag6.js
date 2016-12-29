91fdcb32b00d154f76e4c929c84f3c92d54ded8c
Hector Romero <hector.romero@mataro.epiaedu.cat>
2016-12-29 22:57:46 +0100

    Ejemplo1

--- js/pag6.js
+++ js/pag6.js
@@ -1,7 +1,8 @@
-		
-			function ejemplo1a() {
-								document.getElementById("ejemplo1a").className= "w3-show";
-
-				}
-				
-			
+	function ejemplo1(id) {
+		var x = document.getElementById(id);
+		if (x.className.indexOf("w3-show") == -1) {// busca el numero de veces que aparece w3-show en class. Si no lo encuentra "-1",
+			x.className += " w3-show";// lo añade. Si lo encuentra, lo borra.
+		} else { 
+			x.classList.remove('w3-show');
+		}
+}
