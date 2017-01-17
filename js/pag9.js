3060018073e8e910b88fe061c0f30351af6c7709
Hector Romero <hector.romero@mataro.epiaedu.cat>
2017-01-17 17:54:02 +0100

    Modificaciones ejemplo 1

---
+++ js/pag9.js
@@ -0,0 +1,7 @@
+	function guardar(id) {
+		alert ("Informacion guardada con exito")
+		document.getElementByClass("id").innerHTML =fecha_completa;
+}
+	function borrar(id) {
+		alert ("Esta a punto de borrar el formulario")
+}
