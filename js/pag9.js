f7942ffaacf6375f8be3da8e6993fbbba0fc2207
Hector Romero <hector.romero@mataro.epiaedu.cat>
2017-01-17 19:22:59 +0100

    Modificaciones ejemplo 1

--- js/pag9.js
+++ js/pag9.js
@@ -1,7 +1,11 @@
-	function guardar(id) {
-		alert ("Informacion guardada con exito")
-		document.getElementByClass("id").innerHTML =fecha_completa;
+	function guardar() {
+		var nombre=document.getElementById('nombre').value)
+		if (nombre=="h"){
+			alert ("Error no hay datos en el formulario");
+		}else{
+			alert ("Se han enviado los datos con exito");
+		}	
 }
-	function borrar(id) {
-		alert ("Esta a punto de borrar el formulario")
+	function borrar() {
+		alert ("Esta a punto de borrar el formulario");
 }
