fa135ae70165b464a5a82872c414882aecd48ffc
Hector Romero <hector.romero@mataro.epiaedu.cat>
2017-01-19 00:21:00 +0100

    Primer ejemplo terminado

--- js/pag9.js
+++ js/pag9.js
@@ -1,10 +1,6 @@
 	function guardar() {
-		var nombre=document.getElementById('nombre').value)
-		if (nombre=="h"){
-			alert ("Error no hay datos en el formulario");
-		}else{
 			alert ("Se han enviado los datos con exito");
-		}	
+			
 }
 	function borrar() {
 		alert ("Esta a punto de borrar el formulario");
