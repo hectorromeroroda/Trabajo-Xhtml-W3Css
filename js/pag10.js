0aa10d20989ebdbf2a44b277e213cb5e57da0637
Hector Romero <hector.romero@mataro.epiaedu.cat>
2017-01-23 11:56:38 +0100

    Ejemplo tercero a medias

---
+++ js/pag10.js
@@ -0,0 +1,7 @@
+	function siguiente() {
+			document.getElementById('imagen').src = "imagenes/1.jpg";
+			
+}
+	function atras() {
+		document.getElementById('imagen').src = "imagenes/hielo.jpg";
+}
