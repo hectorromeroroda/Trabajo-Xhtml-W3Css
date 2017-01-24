a43d99fd7021d2268d9bce071729178dcebe2319
Hector Romero <hector.romero@mataro.epiaedu.cat>
2017-01-24 01:18:13 +0100

    Pagina 10 terminada

--- js/pag10.js
+++ js/pag10.js
@@ -1,7 +1,41 @@
+	var contador=0;
+	
 	function siguiente() {
-			document.getElementById('imagen').src = "imagenes/1.jpg";
-			
-}
+			contador++;
+			switch (contador){
+				
+				case 1:
+					document.getElementById('imagen').src = "imagenes/hielo1.jpg";
+					document.getElementById('atras').disabled= false;
+					breack;
+				case 2:
+					document.getElementById('imagen').src = "imagenes/hielo2.jpg";
+					breack;
+				case 3:
+					document.getElementById('imagen').src = "imagenes/hielo3.jpg";
+					document.getElementById('alante').disabled= true;
+					breack;
+				
+				
+			}				
+	}
 	function atras() {
-		document.getElementById('imagen').src = "imagenes/hielo.jpg";
-}
+		contador--;
+			switch (contador){
+				case 0:
+					document.getElementById('imagen').src = "imagenes/hielo.jpg";
+					document.getElementById('atras').disabled= true;
+					breack;
+				case 1:
+					document.getElementById('imagen').src = "imagenes/hielo1.jpg";
+					breack;
+				case 2:
+					document.getElementById('imagen').src = "imagenes/hielo2.jpg";
+					document.getElementById('alante').disabled= false;
+					breack;
+				
+				
+				
+			}				
+	}
+
